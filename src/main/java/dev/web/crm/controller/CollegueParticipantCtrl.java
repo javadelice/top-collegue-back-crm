29a555f370cb852ed592154a94803f7a0633592f
Robin BROQUERIE <robin.broquerie@gmail.com>
2019-07-26 10:45:14 +0200

    Merge branch 'master' into API_PATCH_registration
    # Conflicts:
    #	src/main/java/dev/web/crm/controller/CollegueParticipantCtrl.java
    #	src/main/java/dev/web/crm/service/CollegueParticipantService.java

--- src/main/java/dev/web/crm/controller/CollegueParticipantCtrl.java
+++ src/main/java/dev/web/crm/controller/CollegueParticipantCtrl.java
@@ -5,8 +5,11 @@ import dev.web.crm.dto.Picture;
 import dev.web.crm.service.CollegueParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -14,18 +17,19 @@ public class CollegueParticipantCtrl {
     @Autowired
 	private CollegueParticipantService collegueParticipantService;
 
-	@RequestMapping(method = RequestMethod.PATCH, path = "/registration")
-
+    @Secured("ROLE_USER")
+	@RequestMapping(method = RequestMethod.POST, path = "/registration")
 	public ResponseEntity<?> finaliserInscription(@RequestBody Picture picture) {
 
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        if (picture.getPictureUrl() != null)
-        return collegueParticipantService.finaliserInscription(picture.getPictureUrl(), email);
-		return ResponseEntity.ok().build();
+
+		collegueParticipantService.finaliserInscription(Optional.ofNullable(picture.getPictureUrl()), email);
+
+        return ResponseEntity.ok().build();
 
     }
 
-
+    @Secured("ROLE_USER")
 	@RequestMapping(
             method = RequestMethod.GET,
             path = "/me")
--- src/main/java/dev/web/crm/service/CollegueParticipantService.java
+++ src/main/java/dev/web/crm/service/CollegueParticipantService.java
@@ -2,6 +2,7 @@ package dev.web.crm.service;
 
 import dev.web.crm.dto.CollegueUser;
 import dev.web.crm.entite.CollegueParticipant;
+import dev.web.crm.entite.StatusCollegue;
 import dev.web.crm.exception.CollegueNonTrouveException;
 import dev.web.crm.persistence.CollegueParticipantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,31 +16,32 @@ import java.util.Optional;
 public class CollegueParticipantService {
 
 
-	public CollegueParticipantService() {
-		// TODO Auto-generated constructor stub
-	}
+    public CollegueParticipantService() {
+        // TODO Auto-generated constructor stub
+    }
 
-	@Autowired
-	private CollegueParticipantRepository collegueParticipantRepository;
+    @Autowired
+    private CollegueParticipantRepository collegueParticipantRepository;
 
-	//public CollegueParticipant finaliserInscription(CollegueParticipant pictureUrl) {
-	public ResponseEntity<?> finaliserInscription(String picture, String email) {
-		Optional<CollegueParticipant> cp = collegueParticipantRepository.findByEmail(email);
-         return cp.map(collegue-> {
-        	  collegue.setPictureUrl(picture);
-        	  collegueParticipantRepository.save(collegue);
-        	  return ResponseEntity.status(HttpStatus.CREATED).build();
-          }).orElseGet(() -> {
-        	  return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-          });
+    //public CollegueParticipant finaliserInscription(CollegueParticipant pictureUrl) {
+    public CollegueParticipant finaliserInscription(Optional<String> picture, String email) {
+        return collegueParticipantRepository.findByEmail(email)
+                .map(collegue -> {
+                    picture.ifPresent(collegue::setPictureUrl);
+                    collegue.setStatus(StatusCollegue.SUSCRIBED_CONFIRMED);
+                    collegueParticipantRepository.save(collegue);
+                    return collegue;
+                }).orElseThrow(CollegueNonTrouveException::new);
 
-        }
 
-	public CollegueUser chercherParEmail(String email) {
+    }
+
+    public CollegueUser chercherParEmail(String email) {
         return collegueParticipantRepository.findByEmail(email)
                 .map(c -> new CollegueUser(c.getFirstName(), c.getLastName(), c.getStatus().toString()))
-                .orElseThrow(() -> new CollegueNonTrouveException());
+                .orElseThrow(CollegueNonTrouveException::new);
     }
+
 
 
 }
